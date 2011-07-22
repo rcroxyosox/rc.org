456cf98d7ff542a1053ea431538c45e3bfc9b383
RCONE <robertkcox@gmail.com>
2011-07-21 23:50:42 -0700

    Checked in fonts

--- index.php
+++ index.php
@@ -7,12 +7,15 @@
 		<meta name="generator" content="Adobe GoLive" />
 		<title>rob+is+awesome
 		</title>
-		<style type="text/css" media="screen">
-                    
-                </style>
+		<link rel="stylesheet" type="text/css" href="_lib/css/style.css">
 	</head>
 	<body>
-            Balls
-	</body>
+            <h1>Balls</h1>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor sapien et lacus viverra euismod. Vestibulum cursus sagittis lacus venenatis pretium. Duis magna purus, porttitor id dapibus fermentum, faucibus eu dui. Etiam tempus venenatis enim sed tempus. Nulla et ornare justo. Nam pellentesque nunc ullamcorper diam tristique fermentum. Pellentesque pellentesque pharetra justo, vitae lacinia mauris sollicitudin scelerisque. Fusce eu purus quam. Fusce arcu urna, pharetra sit amet volutpat id, rutrum vel dui. Vestibulum sit amet magna nibh. Maecenas lacus neque, posuere a mollis at, pulvinar non nisl. Suspendisse potenti. Praesent porttitor luctus vestibulum. Etiam eros velit, pretium non sollicitudin at, luctus id orci. Phasellus malesuada adipiscing nunc, fermentum placerat nulla laoreet vitae.</p>
+                <p>Pellentesque placerat lectus in nisl molestie mollis. Praesent ut erat eget magna elementum dignissim id id lectus. Ut viverra pellentesque sem vitae viverra. Cras eros turpis, lobortis sit amet varius ac, tincidunt varius neque. Aliquam turpis orci, rutrum sed venenatis ut, scelerisque sed diam. Nulla lorem tellus, iaculis at tincidunt vitae, dictum a eros. Vivamus quis turpis sit amet ligula sollicitudin vulputate feugiat a diam. Praesent ullamcorper sapien tincidunt augue hendrerit tempor. Sed vitae tellus viverra tortor tristique congue. Sed rutrum elit sit amet nisi tincidunt euismod. Sed nec odio non enim pellentesque lacinia. Ut eget eros et urna sodales gravida. Fusce ac massa eu libero convallis tempus ac eget nisi.</p>
+                <p>Phasellus placerat, diam in euismod congue, urna diam placerat eros, ultrices aliquam lacus nulla ut turpis. Nunc consequat sagittis laoreet. Suspendisse facilisis elit vel metus rhoncus pulvinar. Proin non ligula augue, eu molestie mi. Nulla tincidunt dui eu mi volutpat in fringilla mi pretium. Curabitur nec turpis quam. In sed sem nunc. Vestibulum metus dolor, fermentum vitae iaculis ac, molestie nec leo. In mollis pharetra tempor. Maecenas non elit in ante tempus tristique. Fusce lobortis volutpat libero, id vulputate ante lacinia in.</p>
+            </div>
+        </body>
 
 </html>
